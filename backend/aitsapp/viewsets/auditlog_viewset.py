a6c9fb07b6e857e95a8d179a6ecd9bf2be9b2316
Mayanja Jessy Elijah <mjessyelijah@gmail.com>
2025-04-24 07:25:43 +0300

    deleted fies

--- backend/aitsapp/viewsets/auditlog_viewset.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from auditlog.models import LogEntry
-from ..serializers.auditlog_serializers import AuditLogSerializer
-
-class AuditLogViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = LogEntry.objects.all().order_by("-timestamp")
-    serializer_class = AuditLogSerializer
